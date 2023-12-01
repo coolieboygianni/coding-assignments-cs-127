c39d0de9a3bfbcf3cacdc4be785522905898ef5c
coolieboygianni <143523179+coolieboygianni@users.noreply.github.com>
2023-12-01 18:36:57 -0500

    Add files via upload

---
+++ kmh to mph.cpp
@@ -0,0 +1,15 @@
+//Gianni Russell
+// November 28, 2023
+//gianni.russell@myhunter.cuny.edu
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    float km, miles;
+    cout << "Enter km: ";
+    cin >> km;
+    miles = km * 0.621371;
+    cout << endl << "miles: " << miles << "\n\n";
+    return 0;
+} 
---
+++ mihi cura futuri loop.cpp
@@ -0,0 +1,18 @@
+//Gianni Russell
+//gianni.russell07@myhunter.cuny.edu
+//December 1 , 2023
+//Prints "Mihu Cuta Futuri" 10 times using a loop
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i; 
+    
+    for (i=0; i < 10; i++)
+    {
+        cout << "Mihi cura futuri\n";
+    }
+    return 0;
+}
